595fec9620ede74c8b13059f2c5a10e0967ce5dc
toyxyz <toyxyz@naver.com>
2024-12-31 01:15:42 +0900

    Update attention_couple.py

--- attention_couple.py
+++ attention_couple.py
@@ -59,37 +59,6 @@ def b64image2tensor(img: str, width: int, height: int) -> torch.Tensor:
     return image
 
 
-
-class ImageBatchMulti_2:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "inputcount": ("INT", {"default": 2, "min": 2, "max": 1000, "step": 1}),
-                "image_1": ("IMAGE", ),
-                "image_2": ("IMAGE", ),
-            },
-    }
-
-    RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES = ("images",)
-    FUNCTION = "combine"
-    CATEGORY = "A8R8"
-    DESCRIPTION = """
-Creates an image batch from multiple images.  
-You can set how many inputs the node has,  
-with the **inputcount** and clicking update.
-"""
-
-    def combine(self, inputcount, **kwargs):
-        from nodes import ImageBatch
-        image_batch_node = ImageBatch()
-        image = kwargs["image_1"]
-        for c in range(1, inputcount):
-            new_image = kwargs[f"image_{c + 1}"]
-            image, = image_batch_node.batch(image, new_image)
-        return (image,)
-
 class AttentionCoupleRegion:
     @classmethod
     def INPUT_TYPES(s):
